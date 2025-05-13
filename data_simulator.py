50c16bea5fa6761cdfb3f234ca65e103900c6960
890tsugua <aubhdk@gmail.com>
2025-05-13 16:52:07 +0200

    first commit

---
+++ data_simulator.py
@@ -0,0 +1,336 @@
+# Data simulator
+
+# Basically bare smid ind fra datasimulator...
+
+import numpy as np
+from PIL import Image, ImageDraw
+from PIL import ImageFilter
+import matplotlib.pyplot as plt
+import cv2
+from pathlib import Path
+import pandas as pd
+import tifffile as tiff
+
+# Import PyTorch dependencies
+import torch
+from torch.amp import autocast
+from torch.cuda.amp import GradScaler
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+#from torchtnt.utils import get_module_summary
+import torchvision
+torchvision.disable_beta_transforms_warning()
+from torchvision.tv_tensors import BoundingBoxes, Mask
+from torchvision.utils import draw_bounding_boxes, draw_segmentation_masks
+import torchvision.transforms.v2  as transforms
+from torchvision.transforms.v2 import functional as TF
+
+# Import tqdm for progress bar
+from tqdm.auto import tqdm
+
+""" NOISE GENERATION """
+def add_poisson(array):
+    return np.random.poisson(array).astype(np.float32)
+
+def make_background(base: float = 100, use_gauss_noise: bool = True, sigma: float = 10, pad: int = 20, img_w=512, img_h=512) -> np.float32:
+    img_w += 2*pad
+    img_h += 2*pad
+    if use_gauss_noise:
+        array = np.clip(np.random.normal(base,sigma,(img_h,img_w)), 0, None)
+    else:
+        array = np.full((img_h,img_w),int(base))
+    return array.astype(np.float32)
+
+""" PSF GENERATION """
+def generate_positions(seed: int, num_spots: int, image_width: int, image_height: int):
+    # Set the seed for reproducibility
+    np.random.seed(seed)
+    x = (image_width) * np.random.rand(num_spots)
+    y = (image_height) * np.random.rand(num_spots)
+    positions = np.column_stack((x,y))
+    return positions
+
+def calculate_signal_value(background_mean, snr):
+    return snr * np.sqrt(background_mean)
+
+def make_psf(sigma, intensity, subpos: tuple): 
+    
+    radius = int(np.ceil(3*sigma))
+    array = np.zeros((radius*2+1,radius*2+1))
+    yy, xx = np.ogrid[-radius:radius+1, -radius:radius+1]
+    mask = xx**2 + yy**2 <= radius**2
+    subpos = (subpos[0] - 0.5, subpos[1] - 0.5)
+
+    r2 = (xx-subpos[0])**2 + (yy-subpos[1])**2
+
+    gauss = lambda r2: np.exp(-r2/(2*sigma**2)) # 2D gauss function
+    signals = gauss(r2)
+    # Sum of all pixel values is not always the same... Need to normalize?
+
+    for i in array[mask]:
+        array[mask] = signals[mask] * intensity
+    return array.astype(np.float32), mask
+
+
+""" ADDING PSFS TO AN ARRAY AND DRAWING PICTURE """
+def calculate_background_mean(input_array, sigma, x, y):
+    start_x, start_y = int(np.ceil(x-sigma*3*2)), int(np.ceil(y-sigma*3*2))  # Where to round up??
+    end_x, end_y = int(np.ceil(start_x + sigma*3*2)), int(np.ceil(start_y + sigma*3*2))
+    mean = np.mean(input_array[max(0,start_y):end_y, max(0,start_x):end_x]) # Minimum 0 so that np doesnt access outside array
+    return mean
+
+def round_position(pos):
+    sub_offset = pos % 1
+    shift = sub_offset > 0.5
+    rounded_pos = pos.astype(int) + shift
+    return rounded_pos
+
+def draw_array(positions, sigmas, input_array: np.float32, snrs, pad):
+    h, w = input_array.shape
+    masks = np.zeros((positions.shape[0],h,w)) # Initialize empty masks
+
+    for i, ((x, y), sigma, snr) in enumerate(zip(positions, sigmas, snrs)):
+        subpos = (x%1, y%1)
+        mean = calculate_background_mean(input_array,sigma,pad+x,pad+y)
+        psf, mask = make_psf(sigma, calculate_signal_value(mean, snr), subpos)
+        x, y = x.astype(int), y.astype(int)
+        start_x, start_y = pad+x - int(np.ceil(sigma*3)), pad+y - int(np.ceil(sigma*3))  # Insert PSF input_arrays from their top left corner.
+        input_array[start_y:start_y + psf.shape[0], start_x:start_x + psf.shape[1]] += psf
+
+    # Remove padding and clip
+    unpadded_array = np.clip(input_array[pad:-pad, pad:-pad],0,255)
+    return unpadded_array.astype(np.uint8)
+
+""" MAKING THE TARGETS """
+def make_targets(positions, sigmas, masks, img_w, img_h):
+    """
+    Create targets for the mdoel. Right now boxes are float values. 2x2 sizes."""
+    
+    labels = torch.ones(positions.shape[0]).to(torch.int64)
+
+    bboxes = []
+    #ps = np.floor(positions) # Round down positions.
+    for i in range(positions.shape[0]):
+        #p = ps[i]
+        p = positions[i]
+        #box = [p[0]-1,p[1]-1,p[0]+1,p[1]+1] #xyxy format. Bottom left corner, top right corner.
+        box = [p[0]-1,p[1]-1,p[0]+1,p[1]+1] #xyxy format. Bottom left corner, top right corner.
+        bboxes.append(box)
+    bboxes = torch.tensor(bboxes)
+
+    targets = {
+        'boxes': bboxes,
+        'labels': labels, #torch.Tensor([class_names.index(label) for label in labels]),
+        'positions': torch.tensor(positions).to(torch.float32),
+        'subpixel_positions': torch.tensor(positions%1).to(torch.float32)#torch.tensor(positions).to(torch.float32) / img_w # Between 0 and 1. For now assume square pictures...
+    }
+    return targets
+
+def make_one_data(
+    seed: int = 0,
+    num_spots: int = 5,
+    sigma_mean: float = 2,
+    sigma_std: float = 0.3,
+    snr_mean: float = 10,
+    snr_std: float = 0.3,
+    base_noise: int = 100,
+    use_gauss_noise: bool = True,
+    gauss_noise_std: float = 15,
+    img_w: int = 64,
+    img_h: int = 64):
+    
+    if num_spots == 0:
+            raise ValueError("Number of spots must be more than 0")
+
+    positions = generate_positions(seed, num_spots, img_w, img_h)
+    sigmas = np.clip(np.random.normal(sigma_mean,sigma_std,num_spots),0,None)
+    snrs = np.clip(np.random.normal(snr_mean,snr_std,num_spots),0,None)
+    pad = int(np.ceil(3*np.max(sigmas)*2))
+
+    background = make_background(base_noise,False,gauss_noise_std,pad,img_w, img_h)  # FALSE ADDED
+    image_array = draw_array(positions, sigmas, background, snrs, pad)       
+    #image_array = add_poisson(image_array)                        
+    image_array = np.clip(image_array, 0, 255).astype(np.uint8)
+    image = torch.tensor(np.stack([image_array.astype(np.float32) / 255] * 3, axis=0))  
+
+    targets = make_targets(positions, None, None, img_w, img_h)
+
+    return image, targets
+
+class PsfDataset(Dataset):
+    """
+    This class represents a PyTorch Dataset for a collection of images and their annotations.
+    The class is designed to load images along with their corresponding segmentation masks, bounding box annotations, and labels.
+    """
+    def __init__(self, seed, num_datapoints, 
+                    num_spots_min,
+                    num_spots_max,
+                    sigma_mean,
+                    sigma_std,
+                    snr_mean,
+                    snr_std,
+                    base_noise_min,
+                    base_noise_max,
+                    use_gauss_noise,
+                    gauss_noise_std,
+                    img_w, 
+                    img_h):
+        """
+        Constructor for the HagridDataset class.
+
+        Parameters:
+        img_keys (list): List of unique identifiers for images.
+        annotation_df (DataFrame): DataFrame containing the image annotations.
+        img_dict (dict): Dictionary mapping image identifiers to image file paths.
+        class_to_idx (dict): Dictionary mapping class labels to indices.
+        transforms (callable, optional): Optional transform to be applied on a sample.
+        """
+        super(Dataset, self).__init__()
+        
+        self._len = num_datapoints
+        self._seed = seed
+
+        self._num_spots_min = num_spots_min
+        self._num_spots_max = num_spots_max
+        self._sigma_mean = sigma_mean
+        self._sigma_std = sigma_std
+        self._snr_mean = snr_mean
+        self._snr_std = snr_std
+        self._base_noise_min = base_noise_min
+        self._base_noise_max = base_noise_max
+        self._use_gauss_noise = use_gauss_noise
+        self._gauss_noise_std = gauss_noise_std
+        self._img_w = img_w
+        self._img_h = img_h
+        
+    def __len__(self):
+        """
+        Returns the length of the dataset.
+
+        Returns:
+        int: The number of items in the dataset.
+        """
+        return self._len
+        
+    def __getitem__(self, index):
+
+        #seed = self._seed * self._len + index # Make sure there is a different but unique seed for each data point across epochs. No need...
+        seed = np.random.randint(1,1000000)
+        num_spots = np.random.randint(self._num_spots_min, self._num_spots_max+1)
+        base_noise = np.random.randint(self._base_noise_min, self._base_noise_max+1)
+
+        image, target = make_one_data(  seed,
+                                        num_spots,
+                                        self._sigma_mean,
+                                        self._sigma_std,
+                                        self._snr_mean,
+                                        self._snr_std,
+                                        base_noise,
+                                        self._use_gauss_noise,
+                                        self._gauss_noise_std,
+                                        self._img_w, 
+                                        self._img_h
+                                        )
+        
+        return image, target
+
+def plot_image(image, targets=None, proposals=None, plot_boxes=True, plot_pos=True):
+    # Add a batch dimension
+    image = image.unsqueeze(0)  
+    enlarged_image = F.interpolate(image, scale_factor=10, mode='nearest')
+    img = enlarged_image.squeeze(0) 
+    img = img.cpu()
+    if targets is not None and plot_boxes == True:
+        clrs = []
+        for i in range(targets['boxes'].shape[0]):
+            clrs.append(2)
+        t_boxes = (targets['boxes']*10)
+        t_boxes[:,[2,3]] += 9
+        img = draw_bounding_boxes(img, t_boxes,fill=False, width=1,colors=clrs)
+    if proposals is not None and plot_boxes==True:
+        p_boxes = (proposals['boxes']*10)
+        p_boxes[:,[2,3]] += 9
+        img = draw_bounding_boxes(img,p_boxes,fill=False,width=1)
+    image = img[0].cpu()
+
+    plt.imshow(image)
+    if targets is not None and plot_pos==True:
+        for p, b in zip(targets['subpixel_positions'], targets['boxes']):
+            plt.scatter([(b[0].item()+1+p[0].item())*10],[(b[1].item()+1+p[1].item())*10], color="red", marker="x")
+            #plt.scatter([p[0].item()*64*10],[p[1].item()*64*10], color="red", marker="x")  # Plot a red cross
+    if proposals is not None and plot_pos==True:
+        for p, b in zip(proposals['subpixel_positions'], proposals['boxes']):
+                plt.scatter([(b[0].item()+1+p[0].item())*10],[(b[1].item()+1+p[1].item())*10], edgecolors="blue", marker="o",facecolors="none")
+            #plt.scatter([q[0].item()*64*10],[q[1].item()*64*10], edgecolors="blue", marker="o",facecolors="none") 
+    plt.show()
+
+def plot_image_norm(image, targets=None, proposals=None, plot_boxes=True, plot_pos=True):
+    # Add a batch dimension
+    image = image.unsqueeze(0)  
+    enlarged_image = F.interpolate(image, scale_factor=10, mode='nearest')
+    img = enlarged_image.squeeze(0) 
+    img = img.cpu()
+    if targets is not None and plot_boxes == True:
+        clrs = []
+        for i in range(targets['boxes'].shape[0]):
+            clrs.append(2)
+        t_boxes = (targets['boxes']*10)
+        t_boxes[:,[2,3]] += 9
+        img = draw_bounding_boxes(img, t_boxes,fill=False, width=1,colors=clrs)
+    if proposals is not None and plot_boxes==True:
+        p_boxes = (proposals['boxes']*10)
+        p_boxes[:,[2,3]] += 9
+        img = draw_bounding_boxes(img,p_boxes,fill=False,width=1)
+    image = img[0].cpu()
+
+    plt.imshow(image)
+    if targets is not None and plot_pos==True:
+        for p, b in zip(targets['subpixel_positions'], targets['boxes']):
+            p *= get_box_sizes(b)
+            plt.scatter([(b[0].item()+p[0].item())*10],[(b[1].item()+p[1].item())*10], color="red", marker="x")
+    if proposals is not None and plot_pos==True:
+        for p, b in zip(proposals['subpixel_positions'], proposals['boxes']):
+                p *= get_box_sizes(b)
+                plt.scatter([(b[0].item()+p[0].item())*10],[(b[1].item()+p[1].item())*10], edgecolors="blue", marker="o",facecolors="none")
+            #plt.scatter([q[0].item()*64*10],[q[1].item()*64*10], edgecolors="blue", marker="o",facecolors="none") 
+    plt.show()
+
+def plot_image_boxes(image, targets=None, proposals=None, plot_boxes=True, plot_pos=True):
+    image = image.unsqueeze(0)  
+    enlarged_image = F.interpolate(image, scale_factor=10, mode='nearest')
+    img = enlarged_image.squeeze(0) 
+    img = img.cpu()
+    
+    if targets is not None and plot_boxes == True:
+        clrs = []
+        for i in range(targets['boxes'].shape[0]):
+            clrs.append(2)
+        t_boxes = (targets['boxes']*10)
+        img = draw_bounding_boxes(img, t_boxes,fill=False, width=1,colors=clrs)
+    if proposals is not None and plot_boxes==True:
+        p_boxes = (proposals['boxes']*10)
+        img = draw_bounding_boxes(img,p_boxes,fill=False,width=1)
+    image = img[0].cpu()
+    plt.imshow(image)
+
+    if targets is not None and plot_pos==True:
+        for p in targets['positions']:
+            plt.scatter([p[0].item()*10],[p[1].item()*10], color="red", marker="x")
+    if proposals is not None and plot_pos==True:
+        for p in proposals['subpixel_positions']:
+            plt.scatter([p[0].item()*10],[p[1].item()*10], edgecolors="blue", marker="o",facecolors="none")
+    plt.show()
+
+def get_box_sizes(boxes):
+    if boxes.dim() > 1:
+        widths = boxes[:,2] - boxes[:,0]
+        heights = boxes[:,3] - boxes[:1]
+        box_sizes = torch.stack([widths, heights], dim=1)
+    elif boxes.dim() == 1:
+        widths = boxes[2] - boxes[0]
+        heights = boxes[3] - boxes[1]
+        box_sizes = torch.tensor([widths, heights])
+    else: 
+        raise ValueError("boxes must be 1D or 2D tensor")
+    return box_sizes
---
+++ subpix_rcnn_2.py
@@ -0,0 +1,214 @@
+# subpix_rcnn_2
+
+# Jeg skal i virkeligheden starte ud fra faster_rcnn.py, dvs extende generalized rcnn?? 
+# Problemet er at roi_heads bliver kaldt og defineret inde i faster__rcnn
+# Så skal jeg lave min egen RoIHead. Denne skal nemlig gives som input til generalizedrcnn.
+# Men jeg skal stadig bruge noget af det roihead der findes for faster rcnn... Vil jo stadig godt have bounding boxes til når det
+# skal generaliseres...
+
+# Obs på roi_align. Jeg skal også roi_aligne ground truth subpixel pos for at kunne lave loss function?? 
+# se funktion "project_masks_on_boxes"
+
+from collections import OrderedDict
+from typing import Any, Callable, Optional
+
+import torch.nn as nn
+from torchvision.ops import MultiScaleRoIAlign
+
+from torchvision.ops import misc as misc_nn_ops
+from torchvision.transforms._presets import ObjectDetection
+from torchvision.models._api import register_model, Weights, WeightsEnum
+from torchvision.models._meta import _COCO_CATEGORIES
+from torchvision.models._utils import _ovewrite_value_param, handle_legacy_interface
+from torchvision.models.resnet import resnet50, ResNet50_Weights
+from torchvision.models.detection._utils import overwrite_eps
+from torchvision.models.detection.backbone_utils import _resnet_fpn_extractor, _validate_trainable_layers
+from torchvision.models.detection.faster_rcnn import _default_anchorgen, FasterRCNN, FastRCNNConvFCHead, RPNHead
+from torchvision.models.detection.roi_heads import RoIHeads
+
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from torchvision.models.detection.generalized_rcnn import GeneralizedRCNN
+import torch.nn.functional as F
+import torch
+
+
+class SubpixHead(nn.Module):
+    def __init__(self, in_channels: int):
+        super().__init__()
+        self.head = nn.Sequential(
+            nn.Conv2d(in_channels, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)),  # Reduces to [N, 64, 1, 1]
+            nn.Flatten(),                  # [N, 64]
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 2),              # Output x, y subpixel positions
+            nn.Sigmoid()                   # Values in [0, 1]
+        )
+
+    def forward(self, x):
+        return self.head(x)
+    
+
+class SubpixRoIHeads(RoIHeads):
+    def __init__(self, *args, subpixel_head=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.subpixel_head = subpixel_head
+        self.device = kwargs.get('device', None)
+        print("Custom SubpixRoIHeads successfully initialized!")
+
+    def forward(self, features, proposals, image_shapes, targets = None):
+        result, losses = super().forward(features, proposals, image_shapes, targets) # Results and losses after backbone, rpn and bbox head
+        # Now the subpixel head logic.
+        
+        if self.subpixel_head is not None: # Make sure there is a subpixel head.
+
+            bbox_proposals = [r["boxes"] for r in result] # Extracts bounding boxes. r is for each image.
+
+            # Check if any images don't have boxes... Make the actual lists (not empty) that will be used for training and inference.
+            pooled_boxes = []
+            pooled_image_shapes=[]
+            pooled_offset_targets=[]
+            pooled_indices=[]
+            # Pooled targets: List of tensors shapes [N,2]
+            # Pooled boxes: List of boxes shaped [N,4]
+            # Bbox proposals: List of boxes shaped [N,4] ?? [x1, y1, x2, y2]
+            for i, boxes in enumerate(bbox_proposals):
+                if boxes.shape[0]>0:
+                    pooled_indices.append(i)
+                    pooled_boxes.append(boxes)
+                    pooled_image_shapes.append(image_shapes[i])
+                    if self.training:
+                        if targets is None:
+                            raise ValueError("Targets should not be none when training.")
+                        #pooled_targets.append(targets[i]['subpixel_positions'])
+                        pooled_offset_targets.append(targets[i]['positions']) # [N,2]
+
+                        # I am gonna try implementing normalized offset inside the whole box.
+                        #offsets = (targets[i]['positions'] - boxes[:,[0,1]])
+                        #widths = boxes[:,2] - boxes[:,0] # x2 - x1
+                        #heights = boxes[:,3] - boxes[:,1] # y2 - y1
+                        #box_sizes = torch.stack([widths, heights], dim=1) # [N,2]
+                        #pooled_offset_targets.append(offsets / box_sizes) 
+
+            if len(pooled_boxes) > 0:
+                #subpixel_features = self.box_roi_pool(features,pooled_boxes,pooled_image_shapes) 
+                #subpixel_offsets = self.subpixel_head(subpixel_features)
+                proposed_box_centers = []
+                for boxes in pooled_boxes:
+                    # Compute the center of each box
+                    if boxes.dim() == 1:
+                        boxes = boxes.unsqueeze(0)
+                    pos = torch.stack([(boxes[:,0]+boxes[:,2])/2, (boxes[:,1]+boxes[:,3])/2], dim=1)
+                    proposed_box_centers.append(pos)
+                
+                if self.training:
+                    # Compute subpixel loss.
+                    # subpixel_loss = F.mse_loss(subpixel_offsets, torch.cat(pooled_offset_targets,dim=0)) # Combine targets into one tensor
+                    #losses.update({"loss_subpixel": subpixel_loss})
+                    # Compute position loss
+                    position_loss = F.mse_loss(torch.cat(proposed_box_centers,dim=0), torch.cat(pooled_offset_targets,dim=0)) # Combine targets into one tensor
+                    losses.update({"loss_subpixel": position_loss})
+                else:
+                    # Inference mode. Add the subpixel offset proposals to the results.
+                    num_boxes_per_image = [boxes.shape[0] for boxes in pooled_boxes]
+                    #split_offsets = torch.split(subpixel_offsets,num_boxes_per_image) # Split the subpixel proposals
+                    split_offsets = torch.split(proposed_box_centers,num_boxes_per_image)
+                    for i, offsets in zip(pooled_indices,split_offsets):
+                        result[i]['subpixel_positions'] = offsets
+
+            else: # In case that there are no images with any detections
+                if self.training:
+                    losses.update({"loss_subpixel": torch.tensor(0.0, device=self.device)})
+            
+        return result, losses
+
+
+class SubpixRCNN(FasterRCNN):
+    def __init__(
+        self, 
+        backbone, 
+        num_classes=None, 
+        subpix_in_channels=256, 
+        **kwargs):
+       
+        """
+        Basically the same implementation as the FasterRCNN class, but adds a custom head.
+        kwargs: set specific rpn, box head, transform etc.
+        """
+
+        super().__init__(backbone, num_classes) # Initialize FasterRCNN with default rpn, box head, transform and roi_heads
+        
+        brp=self.roi_heads.box_roi_pool
+        bh=self.roi_heads.box_head
+        bp=self.roi_heads.box_predictor
+
+        # Override the roi_heads
+        subpixel_head = SubpixHead(subpix_in_channels)
+        new_roi_heads = SubpixRoIHeads(
+            box_roi_pool=kwargs.get("box_roi_pool",brp),
+            box_head=kwargs.get("box_head",bh),
+            box_predictor=kwargs.get("box_predictor",bp),
+            fg_iou_thresh=kwargs.get("fg_iou_thresh", 0.5),
+            bg_iou_thresh=kwargs.get("bg_iou_thresh", 0.5),
+            batch_size_per_image=kwargs.get("batch_size_per_image", 512),
+            positive_fraction=kwargs.get("positive_fraction", 0.25),
+            bbox_reg_weights=kwargs.get("bbox_reg_weights", None),
+            score_thresh=kwargs.get("score_thresh", 0.05),
+            nms_thresh=kwargs.get("nms_thresh", 0.5),
+            detections_per_img=kwargs.get("detections_per_img", 100), # No need to pass mask variables, they default to None
+            subpixel_head=subpixel_head
+        )
+        self.roi_heads = new_roi_heads
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                          missing_keys, unexpected_keys, error_msgs):
+        # Call the parent class to load everything else
+        super()._load_from_state_dict(
+            state_dict, prefix, local_metadata, strict,
+            missing_keys, unexpected_keys, error_msgs
+        )
+
+        # Try to load subpixel_head if present
+        subpixel_head_state_dict_keys = [k for k in state_dict.keys() if k.startswith(prefix + "roi_heads.subpixel_head.")]
+        if len(subpixel_head_state_dict_keys) > 0:
+            subpixel_prefix = prefix + "roi_heads.subpixel_head."
+            self.roi_heads.subpixel_head._load_from_state_dict(
+                state_dict, subpixel_prefix, local_metadata, strict,
+                missing_keys, unexpected_keys, error_msgs
+            )
+        else:
+            # If the keys aren't present but strict=True, treat it as missing
+            if strict and self.roi_heads.subpixel_head is not None:
+                m_keys = [prefix + "roi_heads.subpixel_head." + k for k, _ in self.roi_heads.subpixel_head.named_parameters()]
+                missing_keys.extend(m_keys)
+
+
+
+
+"""
+backbone = resnet_fpn_backbone("resnet50", pretrained=True)
+model = SubpixRCNN(backbone, num_classes=2)
+print(model.roi_heads)
+print(model.backbone)
+
+images = [torch.rand(3,800,800) for i in range(10)]
+print(images.__len__())
+
+model.eval()
+output = model(images)
+print(output)"""
+
+
+"""  -- NOTES -- 
+kwargs is passed as a dictionary.
+When initiating a class you can use kwargs.get("key", default)
+
+Inside FasterRCNN: Stuff either has a default value OR inside init is called "if x is none then..."
+
+
+
+
+"""
