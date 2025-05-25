add762f3d94832cc32574291927de03c18d89b92
890tsugua <aubhdk@gmail.com>
2025-05-25 13:50:00 +0200

    Made prediction on Theos data

--- quicktest.py
+++ quicktest.py
@@ -15,15 +15,15 @@ import torchvision.transforms as T
 # Instatiate the dataset
 seed = None
 num_datapoints = 100
-num_spots_min = 5
-num_spots_max = 5
+num_spots_min = 10
+num_spots_max = 10
 sigma_mean= 1.0
 sigma_std = 0.1
 snr_min = 5
 snr_max = 5
 snr_std = 0.0
-base_noise_min = 2000
-base_noise_max = 2000
+base_noise_min = 100
+base_noise_max = 100
 use_gauss_noise = False
 gauss_noise_std = 0.02
 use_perlin_noise = False
@@ -35,33 +35,12 @@ dataset = PsfDataset(seed, num_datapoints, num_spots_min, num_spots_max, sigma_m
                       snr_min, snr_max, snr_std, base_noise_min, base_noise_max, use_gauss_noise,
                       gauss_noise_std, use_perlin_noise, perlin_min_max, img_w, img_h)
 
-# Get a sample
-img, target = dataset[0]
-
-# Tensor to pil image
-img = img.clone().cpu()
-to_pil = T.ToPILImage()
-im = to_pil(img)
-# Convert to grayscale
-im = im.convert("L")
-im.save("/Users/august/Desktop/bachelor/bachelor1/testhighbg.png")
-
-print(target)
-PlotController(img, target, None, 'buttons', False, 1, False)
-
-# Save the image
-
-
-
-#import numpy as np
-#a = np.zeros((10,10))
-#a[1:3, 1:3] = 1
-#print(a)
-
-#sim = PsfSimulator(use_perlin_noise=True, perlin_min_max=(0.4,0.6), sigma_mean=1, snr_mean=5)
-#psf = sim.make_psf(0.8, 250, (0.5,0.5))
-#import PIL
-#from PIL import Image
-#im = Image.fromarray(psf)
-#im = im.convert("L")
-#im.show()
+image, target = dataset[0]
+snrs = target['true_snrs']
+# Plot the distribution of snrs
+import matplotlib.pyplot as plt
+plt.hist(snrs, bins=50, edgecolor='black')
+plt.xlabel('SNR')
+plt.ylabel('Frequency')
+plt.title('Distribution of SNRs')
+plt.show()
--- testing.py
+++ testing.py
@@ -14,96 +14,79 @@ import torchvision.transforms as T
 import pickle
 import time
 import matplotlib.pyplot as plt
+import tifffile
+import numpy as np
 
 device = 'cuda'
-seed = None
-num_datapoints = 1
-num_spots_min = 1
-num_spots_max = 1
-sigma_mean= 1.0
-sigma_std = 0.1
-snr_min = 8
-snr_max = 8
-snr_std = 0.0
-base_noise_min = 1000
-base_noise_max = 1000
-use_gauss_noise = False
-gauss_noise_std = 0.02
-use_perlin_noise = False
-perlin_min_max = (0.4, 0.6)
-img_w = 64
-img_h = 64
 
-sim = PsfSimulator(snr_mean=5, base_noise=6000)
-t0 = time.time()
-image, target = sim.generate(num_spots=20)
-print(f"Time taken to generate image: {time.time() - t0:.3f} seconds")
-print(target)
+### LOAD THE MODEL ###
+backbone = resnet_fpn_backbone("resnet50", pretrained=True)
+kwargs = {"nms_thresh": 0.1, "detections_per_img": None, "score_thresh": 0.9}
+model = SubpixRCNN(backbone, num_classes=2, device=device, **kwargs)
+model.to(device=device)
+path = r"D:\zeiss\Desktop\coding\Hilger\bachelor\subpix_rcnn_models\2025-05-24_13-46-15\second_long_run.pth"
+model.load_state_dict(torch.load(path, map_location=device))
 
-# print(array.max())
-# plt.imshow(array, cmap='gray')
-# plt.colorbar()  # Optional: shows a color scale
-# plt.title("Array Plot")
-# plt.show()
-PlotController(image, target, None, 'buttons', 0, 1, 1)
-
+### FOR SIMULATING DATA AND TESTING IT ON THE MODEL ###
+# seed = None
+# num_datapoints = 1
+# num_spots_min = 100
+# num_spots_max = 100
+# sigma_mean= 1.0
+# sigma_std = 0.1
+# snr_min = 3
+# snr_max = 3
+# snr_std = 0.0
+# base_noise_min = 5000
+# base_noise_max = 5000
+# use_gauss_noise = False
+# gauss_noise_std = 0.02
+# use_perlin_noise = False
+# perlin_min_max = (0.4, 0.6)
+# img_w = 64
+# img_h = 64
 # dataset = PsfDataset(seed, num_datapoints, num_spots_min, num_spots_max, sigma_mean, sigma_std,
 #                       snr_min, snr_max, snr_std, base_noise_min, base_noise_max, use_gauss_noise,
 #                       gauss_noise_std, use_perlin_noise, perlin_min_max, img_w, img_h)
 # image, target = dataset[0]
 # image = move_data_to_device(image, device)
 # target = move_data_to_device(target, device)
-
-# backbone = resnet_fpn_backbone("resnet50", pretrained=True)
-# kwargs = {"nms_thresh": 0.1, "detections_per_img": None, "score_thresh": 0.9}
-# model = SubpixRCNN(backbone, num_classes=2, device=device, **kwargs)
-# model.to(device=device)
-# path = r"/Users/august/Desktop/bachelor/bachelor1/first_long_run.pth"
-# model.load_state_dict(torch.load(path, map_location=device))
 # model.eval()
-
 # with torch.no_grad():
 #     pred = model([image.to(device)])
 # pred = pred[0]
 # pred = move_dict_to_cpu(pred)
 # target = move_dict_to_cpu(target)
 # print(target['positions'], target['true_snrs'])
-
 # PlotController(image, target, pred, 'eval', 1, 1, 1)
 
 
+### FOR TESTING MODEL ON REAL DATA ###
+# Open the tif file
+tif_path = r"D:\zeiss\Desktop\coding\Hilger\bachelor\theo_frame_504.tif"
+image = tifffile.imread(tif_path)
+bcimage = image.copy()
+#image = np.clip(image, 0, 10000)
+image /= np.max(image)  # Normalize the image to [0, 1]
 
+image_tensor = torch.tensor(image, dtype=torch.float32)
+# Make it three channels
+image_tensor = image_tensor.unsqueeze(0).repeat(3, 1, 1)  # [C, H, W]
+image_tensor = move_data_to_device(image_tensor, device)
+model.eval()
+with torch.no_grad():
+    pred = model([image_tensor])
+pred = pred[0]
+pred = move_dict_to_cpu(pred)
 
-# img_path = r"D:\zeiss\Desktop\coding\Hilger\bachelor\screenshot.png"
-# pil_img = Image.open(img_path).convert("L")  # "L" = grayscale
+# Increase contrast and brightness for visualization. Not sure how its done but it works
+bcimage = np.clip(bcimage, 0, 4200)
+bcimage /= np.max(bcimage)  # Normalize the image to [0, 1]
+bcimage_tensor = torch.tensor(bcimage, dtype=torch.float32)
+bcimage_tensor = bcimage_tensor.unsqueeze(0).repeat(3, 1, 1)  # [C, H, W]
+bcimage_tensor = move_data_to_device(bcimage_tensor, device)
 
-# # Downscale by 0.5x to revert retina doubling
-# w, h = pil_img.size
-# pil_img = pil_img.resize((w // 2, h // 2), Image.LANCZOS)
-
-# # Convert to 3 channels by repeating
-# to_tensor = T.ToTensor()
-# img_tensor = to_tensor(pil_img)  # shape: (1, H, W)
-# img_tensor = img_tensor.repeat(3, 1, 1)  # shape: (3, H, W)
-
-# t0 = time.time()
-# with torch.no_grad():
-#     pred = model([img_tensor.to(device)])
-# print(f"Prediction time: {time.time() - t0:.3f} seconds")
-# pred = pred[0]
-# theo_pred = move_dict_to_cpu(pred)
-
-
-# # save_path = r'd:\zeiss\Desktop\coding\Hilger\bachelor\theo_pred.pkl'
-
-# # with open(save_path, 'wb') as f:
-# #     pickle.dump(theo_pred, f)
-
-# PlotController(img_tensor, None, theo_pred, 'buttons', 1,0,1)
-
-
-
-
+PlotController(bcimage_tensor, None, pred, 'buttons', 1, 0, 1)
 
 
 
